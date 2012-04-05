52e7b07b70d4e4cc5aedd1be813449d4583ecd12
Thomas Chust <chust@web.de>
2012-04-05 16:37:22 +0200

    Environment configuration using descriptor records instead of flags

--- db/environment.go
+++ db/environment.go
@@ -18,6 +18,16 @@ import (
  */
 import "C"
 
+// Database environment configuration.
+type EnvironmentConfig struct {
+	Mode os.FileMode    // File creation mode for the environment.
+	Create bool         // Create the environment, if necessary.
+	Recover bool        // Run recovery on the environment, if necessary.
+	Transactional bool  // Enable transactions in the environment.
+	NoSync bool         // Do not flush to log when committing.
+	WriteNoSync bool    // Do not flush log when committing.
+}
+
 // Database environment.
 type Environment struct {
 	ptr *C.DB_ENV
@@ -27,21 +37,43 @@ type Environment struct {
 var NoEnvironment = Environment{ptr: nil}
 
 // Open an environment at the given home path.
-func OpenEnvironment(home string, flags uint32, mode os.FileMode) (env Environment, err error) {
+func OpenEnvironment(home string, config *EnvironmentConfig) (env Environment, err error) {
 	err = check(C.db_env_create(&env.ptr, 0))
 	if err != nil {
 		return
 	}
 
-	chome := C.CString(home)
-	err = check(C.db_env_open(env.ptr, chome, C.u_int32_t(flags), C.int(mode)))
+	var chome *C.char = C.CString(home)
+	var flags C.u_int32_t = C.DB_THREAD
+	var mode C.int = 0755
+	if config != nil {
+		mode = C.int(config.Mode)
+		if config.Create {
+			flags |= C.DB_CREATE
+		}
+		if config.Recover {
+			flags |= C.DB_REGISTER | C.DB_FAILCHK | C.DB_RECOVER
+		}
+		if config.Transactional {
+			flags |= C.DB_INIT_TXN | C.DB_INIT_MPOOL
+		}
+		if config.NoSync {
+			flags |= C.DB_TXN_NOSYNC
+		}
+		if config.WriteNoSync {
+			flags |= C.DB_TXN_WRITE_NOSYNC
+		}
+	}
+
+	err = check(C.db_env_open(env.ptr, chome, flags, mode))
+
 	C.free(unsafe.Pointer(chome))
 
 	return
 }
 
 // Close the environment.
-func (env Environment) Close(flags uint32) (err error) {
-	err = check(C.db_env_close(env.ptr, C.u_int32_t(flags)))
+func (env Environment) Close() (err error) {
+	err = check(C.db_env_close(env.ptr, C.u_int32_t(C.DB_FORCESYNC)))
 	return
 }
