5a1bc9fd701b8005ccc99a90c8b3e885fb34c29a
Thomas Chust <chust@web.de>
2012-04-07 17:42:20 +0200

    Reworked the set of database operations

--- db/database.go
+++ db/database.go
@@ -1,10 +1,9 @@
 package db
 
 import (
-	"os"
-	"reflect"
-	"unsafe"
 	"code.google.com/p/goprotobuf/proto"
+	"os"
+	"unsafe"
 )
 
 /*
@@ -23,6 +22,9 @@ import (
  static inline int db_put(DB *db, DB_TXN *txn, DBT *key, DBT *data, u_int32_t flags) {
  	return db->put(db, txn, key, data, flags);
  }
+ static inline int db_get(DB *db, DB_TXN *txn, DBT *key, DBT *data, u_int32_t flags) {
+ 	return db->get(db, txn, key, data, flags);
+ }
  static inline int db_del(DB *db, DB_TXN *txn, DBT *key, u_int32_t flags) {
  	return db->del(db, txn, key, flags);
  }
@@ -35,7 +37,10 @@ import (
  static inline int db_cursor_get(DBC *cur, DBT *key, DBT *data, u_int32_t flags) {
  	return cur->get(cur, key, data, flags);
  }
- */
+ static inline int db_cursor_del(DBC *cur, u_int32_t flags) {
+ 	return cur->del(cur, flags);
+ }
+*/
 import "C"
 
 // Type of databases.
@@ -43,19 +48,19 @@ type DatabaseType int
 
 // Available database types.
 const (
-	BTree = DatabaseType(C.DB_BTREE)
-	Hash = DatabaseType(C.DB_HASH)
+	BTree   = DatabaseType(C.DB_BTREE)
+	Hash    = DatabaseType(C.DB_HASH)
 	Records = DatabaseType(C.DB_RECNO)
-	Queue = DatabaseType(C.DB_QUEUE)
+	Queue   = DatabaseType(C.DB_QUEUE)
 	Unknown = DatabaseType(C.DB_UNKNOWN)
 )
 
 // Database configuration.
 type DatabaseConfig struct {
-	Mode os.FileMode      // File creation mode for the environment.
-	Create bool           // Create the database, if necessary.
-	ReadUncommitted bool  // Enable support for read-uncommitted isolation.
-	Snapshot bool         // Enable support for snapshot isolation.
+	Mode            os.FileMode // File creation mode for the environment.
+	Create          bool        // Create the database, if necessary.
+	ReadUncommitted bool        // Enable support for read-uncommitted isolation.
+	Snapshot        bool        // Enable support for snapshot isolation.
 }
 
 // Database.
@@ -115,194 +120,295 @@ func (db Database) DatabaseType() (dbtype DatabaseType, err error) {
 	return
 }
 
-// Store some values under the given key in the database. For Records
-// and Queue databases, the key must be a pointer to a RecordId
-// variable; in append mode that variable will be set to the new
-// record number.
-func (db Database) Insert(txn Transaction, flags uint32, key interface{}, data ...interface{}) (err error) {
-	var buf []byte
-	var ckey, cdata C.DBT
+// Interface for storable records.
+type Record interface {
+	// Obtain a pointer to the record key. If the record currently
+	// has no key, it must be allocated. The result must be
+	// serializable using protobuf or, for storage in queue and
+	// records databases, a *uint32.
+	RecordKey() interface{}
 
-	if flags & Append == 0 {
-		buf, err = proto.Marshal(key)
-		if err != nil {
-			return
-		}
+	// Obtain a pointer to a copy of the record without its
+	// key. The result must be serializable using protobuf.
+	RecordWithoutKey() interface{}
+}
 
-		if ckey.size = C.u_int32_t(len(buf)); ckey.size > 0 {
-			ckey.data = unsafe.Pointer(&buf[0])
-		}
+// Marshal a protobuf struct into a database thang.
+func marshalDBT(dbt *C.DBT, val interface{}) (err error) {
+	buf, err := proto.Marshal(val)
+	if err != nil {
+		return
 	}
 
-	for _, dati := range data {
-		buf, err = proto.Marshal(dati)
-		if err != nil {
-			return
-		}
+	if len(buf) > 0 {
+		dbt.data = unsafe.Pointer(&buf[0])
+		dbt.size = C.u_int32_t(len(buf))
+	} else {
+		dbt.data = nil
+		dbt.size = 0
+	}
 
-		if cdata.size = C.u_int32_t(len(buf)); cdata.size > 0 {
-			cdata.data = unsafe.Pointer(&buf[0])
+	return
+}
+
+// Marshal the key of a record into a database thang.
+func (db Database) marshalKey(dbt *C.DBT, rec Record) (err error) {
+	dbtype, err := db.DatabaseType()
+	if err != nil {
+		return
+	}
+
+	switch dbtype {
+	case Records, Queue:
+		dbt.data = unsafe.Pointer(rec.RecordKey().(*uint32))
+		dbt.size = 4
+
+	default:
+		err = marshalDBT(dbt, rec.RecordKey())
+	}
+
+	return
+}
+
+// Marshal the data of a record into a database thang.
+func (db Database) marshalData(dbt *C.DBT, rec Record) (err error) {
+	err = marshalDBT(dbt, rec.RecordWithoutKey())
+	return
+}
+
+// Unmarshal a protobuf struct from a database thang.
+func unmarshalDBT(dbt *C.DBT, val interface{}) (err error) {
+	buf := C.GoBytes(dbt.data, C.int(dbt.size))
+	err = proto.Unmarshal(buf, val)
+	return
+}
+
+// Unmarshal the key of a record from a database thang.
+func (db Database) unmarshalKey(dbt *C.DBT, rec Record) (err error) {
+	dbtype, err := db.DatabaseType()
+	if err != nil {
+		return
+	}
+
+	switch dbtype {
+	case Records, Queue:
+		if dbt.size == 4 {
+			*rec.RecordKey().(*uint32) = *(*uint32)(dbt.data)
 		} else {
-			cdata.data = nil
+			panic("key size does not match record number data type")
 		}
 
-		err = check(C.db_put(db.ptr, txn.ptr, &ckey, &cdata, C.u_int32_t(flags)))
-		if err != nil {
-			return
-		}
-	}
-
-	if flags & Append != 0 {
-		buf = C.GoBytes(ckey.data, C.int(ckey.size))
-		err = proto.Unmarshal(buf, key)
+	default:
+		err = unmarshalDBT(dbt, rec.RecordKey())
 	}
 
 	return
 }
 
-// Remove a key and its associated values from the database. For
-// Records and Queue databases, the key must be a pointer to a
-// RecordId variable.
-func (db Database) Remove(txn Transaction, flags uint32, key interface{}) (err error) {
-	var buf []byte
-	var ckey C.DBT
+// Unmarshal the data of a record from a database thang.
+func (db Database) unmarshalData(dbt *C.DBT, rec Record) (err error) {
+	err = unmarshalDBT(dbt, rec)
+	return
+}
 
-	buf, err = proto.Marshal(key)
+// Store records in the database. The append flag makes sense only in
+// combination with a queue or records database and causes the keys of
+// the records to be set to fresh record numbers.
+func (db Database) Put(txn Transaction, append bool, recs ...Record) (err error) {
+	var flags C.u_int32_t
+	if append {
+		flags = C.DB_APPEND
+	} else {
+		flags = 0
+	}
+
+	var key, data C.DBT
+	for _, rec := range recs {
+		err = db.marshalKey(&key, rec)
+		if err != nil {
+			return
+		}
+
+		err = db.marshalData(&data, rec)
+		if err != nil {
+			return
+		}
+
+		err = check(C.db_put(db.ptr, txn.ptr, &key, &data, flags))
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+// Get records from the database. The consume flag makes sense only in
+// combination with a queue database and causes the operation to wait
+// for and obtain the next enqueued record.
+func (db Database) Get(txn Transaction, consume bool, recs ...Record) (err error) {
+	var flags C.u_int32_t
+	if consume {
+		flags = C.DB_CONSUME_WAIT
+	} else {
+		flags = 0
+	}
+
+	var key, data C.DBT
+	for _, rec := range recs {
+		err = db.marshalKey(&key, rec)
+		if err != nil {
+			return
+		}
+
+		err = check(C.db_get(db.ptr, txn.ptr, &key, &data, flags))
+		if err != nil {
+			return
+		}
+
+		err = db.unmarshalData(&data, rec)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+// Delete records from the database.
+func (db Database) Del(txn Transaction, recs ...Record) (err error) {
+	var key C.DBT
+	for _, rec := range recs {
+		err = db.marshalKey(&key, rec)
+		if err != nil {
+			return
+		}
+
+		err = check(C.db_del(db.ptr, txn.ptr, &key, 0))
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+// Database cursor.
+type Cursor struct {
+	Database
+	ptr *C.DBC
+}
+
+// Obtain a cursor over the database.
+func (db Database) Cursor(txn Transaction) (cur Cursor, err error) {
+	cur.Database = db
+	err = check(C.db_cursor(db.ptr, txn.ptr, &cur.ptr, 0))
+	return
+}
+
+// Close the cursor.
+func (cur Cursor) Close() (err error) {
+	err = check(C.db_cursor_close(cur.ptr))
+	return
+}
+
+// Retrieve the first record with matching key from the database.
+func (cur Cursor) Set(rec Record) (err error) {
+	var key, data C.DBT
+
+	err = cur.marshalKey(&key, rec)
 	if err != nil {
 		return
 	}
 
-	if ckey.size = C.u_int32_t(len(buf)); ckey.size > 0 {
-		ckey.data = unsafe.Pointer(&buf[0])
-	}
-
-	err = check(C.db_del(db.ptr, txn.ptr, &ckey, C.u_int32_t(flags)))
-	return
-}
-
-// Query object.
-type Query struct {
-	db Database
-	cur *C.DBC
-	key interface{}
-}
-
-// Initialize a query for the given key.
-func (db Database) Find(txn Transaction, flags uint32, key interface{}) (qry *Query, err error) {
-	if key != nil {
-		qry = &Query{db: db, key: key}
-	} else {
-		qry = &Query{db: db}
-	}
-	err = check(C.db_cursor(db.ptr, txn.ptr, &qry.cur, C.u_int32_t(flags)))
-	return
-}
-
-// Finish a query.
-func (qry *Query) Close() (err error) {
-	err = check(C.db_cursor_close(qry.cur))
-	return
-}
-
-// Extract the next duplicate from the query.
-func (qry *Query) One(data interface{}) (err error) {
-	var buf []byte
-	var ckey, cdata C.DBT
-	var flags uint32
-
-	if qry.key != nil {
-		buf, err = proto.Marshal(qry.key)
-		if err != nil {
-			return
-		}
-
-		if ckey.size = C.u_int32_t(len(buf)); ckey.size > 0 {
-			ckey.data = unsafe.Pointer(&buf[0])
-		}
-
-		flags = C.DB_SET
-	} else {
-		flags = C.DB_NEXT_DUP
-	}
-
-	err = check(C.db_cursor_get(qry.cur, &ckey, &cdata, C.u_int32_t(flags)))
-	if err == nil {
-		qry.key = nil
-	} else {
-		return
-	}
-
-	buf = C.GoBytes(cdata.data, C.int(cdata.size))
-	err = proto.Unmarshal(buf, data)
-
-	return
-}
-
-// Extract all duplicates from the query.
-func (qry *Query) All(data interface{}) (err error) {
-	datav := reflect.ValueOf(data)
-	if datav.Kind() != reflect.Ptr || datav.Elem().Kind() != reflect.Slice {
-		panic("data argument must be a slice address")
-	}
-
-	slicev := datav.Elem().Slice(0, 0)
-	elemt := slicev.Type().Elem()
-	for {
-		elemv := reflect.New(elemt)
-		err = qry.One(elemv.Interface())
-		switch err {
-		case nil:
-			slicev = reflect.Append(slicev, elemv.Elem())
-
-		case ErrNotFound:
-			err = nil
-			break
-
-		default:
-			return
-		}
-	}
-
-	datav.Elem().Set(slicev)
-	return
-}
-
-// Extract the next key and data from the query.
-func (qry *Query) Next(key interface{}, data interface{}) (err error) {
-	var buf []byte
-	var ckey, cdata C.DBT
-	var flags uint32
-
-	if qry.key != nil {
-		buf, err = proto.Marshal(qry.key)
-		if err != nil {
-			return
-		}
-
-		if ckey.size = C.u_int32_t(len(buf)); ckey.size > 0 {
-			ckey.data = unsafe.Pointer(&buf[0])
-		}
-
-		flags = C.DB_SET
-	} else {
-		flags = C.DB_NEXT
-	}
-
-	err = check(C.db_cursor_get(qry.cur, &ckey, &cdata, C.u_int32_t(flags)))
-	if err == nil {
-		qry.key = nil
-	} else {
-		return
-	}
-
-	buf = C.GoBytes(ckey.data, C.int(ckey.size))
-	err = proto.Unmarshal(buf, key)
+	err = check(C.db_cursor_get(cur.ptr, &key, &data, C.DB_SET))
 	if err != nil {
 		return
 	}
 
-	buf = C.GoBytes(cdata.data, C.int(cdata.size))
-	err = proto.Unmarshal(buf, data)
+	err = cur.unmarshalData(&data, rec)
 
+	return
+}
+
+// Retrieve the first record of the database.
+func (cur Cursor) First(rec Record) (err error) {
+	var key, data C.DBT
+
+	err = check(C.db_cursor_get(cur.ptr, &key, &data, C.DB_FIRST))
+	if err != nil {
+		return
+	}
+
+	err = cur.unmarshalKey(&key, rec)
+	if err != nil {
+		return
+	}
+
+	err = cur.unmarshalData(&data, rec)
+
+	return
+}
+
+// Retrieve the next record from the cursor.
+func (cur Cursor) Next(rec Record) (err error) {
+	var key, data C.DBT
+
+	err = check(C.db_cursor_get(cur.ptr, &key, &data, C.DB_NEXT))
+	if err != nil {
+		return
+	}
+
+	err = cur.unmarshalKey(&key, rec)
+	if err != nil {
+		return
+	}
+
+	err = cur.unmarshalData(&data, rec)
+
+	return
+}
+
+// Retrieve the last record of the database.
+func (cur Cursor) Last(rec Record) (err error) {
+	var key, data C.DBT
+
+	err = check(C.db_cursor_get(cur.ptr, &key, &data, C.DB_LAST))
+	if err != nil {
+		return
+	}
+
+	err = cur.unmarshalKey(&key, rec)
+	if err != nil {
+		return
+	}
+
+	err = cur.unmarshalData(&data, rec)
+
+	return
+}
+
+// Retrieve the previous record from the cursor.
+func (cur Cursor) Prev(rec Record) (err error) {
+	var key, data C.DBT
+
+	err = check(C.db_cursor_get(cur.ptr, &key, &data, C.DB_PREV))
+	if err != nil {
+		return
+	}
+
+	err = cur.unmarshalKey(&key, rec)
+	if err != nil {
+		return
+	}
+
+	err = cur.unmarshalData(&data, rec)
+
+	return
+}
+
+// Delete the current record at the cursor.
+func (cur Cursor) Del() (err error) {
+	err = check(C.db_cursor_del(cur.ptr, 0))
 	return
 }
